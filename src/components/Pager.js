e4bca38896d7607739d3a0ada6e01c1dac35cddd
Kelli Warner <kbwarner@gmail.com>
2020-01-27 21:23:58 -0500

    added pager

--- src/components/Pager.js
+++ src/components/Pager.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function Pager(props) {
-  return (
-    <div className="pagination">
-      {() => {
-        while (p < Math.ceil(props.count / 50)) {
-          <a></a>;
-        }
-      }}
-    </div>
-  );
+  const pages = Math.ceil(props.count / 50);
+
+  let pagerMapping = [];
+  for (let i = 0; i < pages; i++) {
+    pagerMapping.push(<a href={`#view=users&idx=${i}`}>{i + 1}</a>);
+  }
+
+  return <div className="pagination">{pagerMapping}</div>;
 }
 
 export default Pager;
