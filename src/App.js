6d6f582766f7f8c9d60f554aed2a2ec52de42ab9
Judas <pleasedontforgetme99@gmail.com>
2020-01-27 19:37:22 -0500

    make router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import qs from "qs";
+import { getHash } from "./utils/utils";
+//Import Components
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Users from "./components/Users";
 
 function App() {
-  const getHash = () => {
-    return window.location.hash.slice(1);
-  };
   const [params, setParams] = useState(qs.parse(getHash()));
   useEffect(() => {
     window.addEventListener("hashchange", () => {
@@ -13,7 +15,14 @@ function App() {
     setParams(qs.parse(getHash()));
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+
+      {params.view === undefined && <Home />}
+      {params.view === "users" && <Users />}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav-box">
+      <a href="#">Home</a>
+      <a href="#view=users">Users</a>
+    </div>
+  );
+}
+
+export default Nav;
