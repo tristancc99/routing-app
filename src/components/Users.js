31097552507e929fce7c89f7557be8b94bd02e3e
Kelli Warner <kbwarner@gmail.com>
2020-01-27 20:16:26 -0500

    updated users

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API = `http://acme-users-api-rev.herokuapp.com/api/users`;
 
 function Users() {
-  return <div>Users</div>;
+  let [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    axios.get(API).then(response => setUserList(response.data.users));
+  }, []);
+
+  console.log(userList);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {userList.map(user => (
+          <li>{user.fullName}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Users;
